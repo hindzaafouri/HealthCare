095f25dad2472c4b2cb772d15b9f768d2ef4eb07
hind <hind.zaafouri@esprit.tn>
2023-04-08 01:46:27 +0100

    added frontend project

--- HealthCare_frontEnd/karma.conf.js
+++ HealthCare_frontEnd/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/health-care-front'),
+      dir: require('path').join(__dirname, './coverage/health-care-front-end'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- HealthCare_frontEnd/src/app/app.component.ts
+++ HealthCare_frontEnd/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'healthcare_front';
+  title = 'HealthCare_frontEnd';
 }
