6e4f5405ce0494659ac12ac32f4a364b79a30405
Cyrine Antar <cyrine.antar@esprit.tn>
2023-05-01 18:01:01 +0200

    front done

--- HealthCare_client/src/app/app-routing.module.ts
+++ HealthCare_client/src/app/app-routing.module.ts
@@ -4,11 +4,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { AllTemplateClientComponent } from './frontOffice/all-template-client/all-template-client.component';
 import { ThreadComponent } from './frontOffice/thread/thread.component';
 import { ThreadDetailsComponent } from './frontOffice/thread-details/thread-details.component';
+import { HomeComponent } from './frontOffice/home/home/home.component';
+import { AddAppointmentComponent } from './frontOffice/Appointment/add-appointment/add-appointment.component';
+import { HeaderBackComponent } from './backOffice/header-back/header-back.component';
+import { BodyComponent } from './frontOffice/body/body.component';
+import { ShowAppointmentComponent } from './frontOffice/Appointment/show-appointment/show-appointment.component';
+import { DetailsShowComponent } from './frontOffice/Appointment/details-show/details-show.component';
+import { EditAppointmentComponent } from './frontOffice/Appointment/edit-appointment/edit-appointment.component';
 
 const routes: Routes = [
     { path: "front" , component: AllTemplateClientComponent},
   { path : 'threads'  , component:ThreadComponent },
-  { path : 'threads/:idThread', component:ThreadDetailsComponent }
+  { path : 'threads/:idThread', component:ThreadDetailsComponent },
+  { path: "" , component: HomeComponent},
+  { path : 'Appointment' , component : AddAppointmentComponent},
+  { path : 'back' , component : HeaderBackComponent},
+  { path : 'Appointment/show' , component : ShowAppointmentComponent},
+  { path : 'Appointment/show/details' , component : DetailsShowComponent},
+  { path : 'editappointment/:id_appointment' , component : EditAppointmentComponent},
 ];
 
 
--- HealthCare_client/src/app/app.module.ts
+++ HealthCare_client/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule , Routes} from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AllTemplateClientComponent } from './frontOffice/all-template-client/all-template-client.component';
 import { FooterComponent } from './frontOffice/footer/footer.component';
@@ -11,6 +11,14 @@ import { BodyComponent } from './frontOffice/body/body.component';
 import { HeaderComponent } from './frontOffice/header/header.component';
 import { ThreadComponent } from './frontOffice/thread/thread.component';
 import { ThreadDetailsComponent } from './frontOffice/thread-details/thread-details.component';
+import { HomeComponent } from './frontOffice/home/home/home.component';
+import { ToastrModule } from 'ngx-toastr';
+import { AddAppointmentComponent } from './frontOffice/Appointment/add-appointment/add-appointment.component';
+import { EditAppointmentComponent } from './frontOffice/Appointment/edit-appointment/edit-appointment.component';
+import { ShowAppointmentComponent } from './frontOffice/Appointment/show-appointment/show-appointment.component';
+import { DetailsShowComponent } from './frontOffice/Appointment/details-show/details-show.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
@@ -21,13 +29,26 @@ import { ThreadDetailsComponent } from './frontOffice/thread-details/thread-deta
     HeaderComponent,
     ThreadComponent,
     ThreadDetailsComponent,
+    HomeComponent,
+    AddAppointmentComponent,
+    EditAppointmentComponent,
+    ShowAppointmentComponent,
+    DetailsShowComponent
   ],
   imports: [
+    NgxPaginationModule,
+    NgbModule,
     BrowserModule,
     HttpClientModule,
     FormsModule,
     RouterModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      timeOut: 5000,
+      progressBar: true,
+      progressAnimation: 'increasing'
+    }),
   ],
   exports: [RouterModule],
   providers: [],
